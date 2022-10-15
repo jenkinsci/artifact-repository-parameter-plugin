4df07e16c20cc94e5862f89c74d1d37762c77402
frieder.heugel@straumann.com <NggB1SuIjOni-oJLyaHd>
2022-10-15 07:51:20 +0200

    Issue-14 - Revert to plain JS, remove jQuery3

--- src/main/webapp/js/config.js
+++ src/main/webapp/js/config.js
@@ -1,43 +1,27 @@
 /**
- * These scripts are used in config.jelly
+ * This function is used in config.jelly
  *
- * Depending on the selection of the parameter type in the API Options section different blocks
- * are displayed/hidden.
+ * Depending on the selection of the parameter type in the API Options section different
+ * options are displayed/hidden.
  */
+function showOptions(element) {
+    const value = element.value;
+    const parent = element.closest("div.jenkins-section");
+    const pathOption = parent.querySelector("div.arpApiOptionPath");
+    const versionOption = parent.querySelector("div.arpApiOptionVersion");
+    const repoOption = parent.querySelector("div.arpApiOptionRepository");
 
-function changeOptionsVisibility(option) {
-    const selection = jQuery3(option).val();
-    const parentSection = jQuery3(option).parents("div.jenkins-section");
-
-    const path = jQuery3(parentSection).find("div.arpApiOptionPath");
-    const version = jQuery3(parentSection).find("div.arpApiOptionVersion");
-    const repo = jQuery3(parentSection).find("div.arpApiOptionRepository");
-
-    switch (selection) {
-        case "path":
-            path.show();
-            version.hide();
-            repo.hide();
-            break;
-        case "version":
-            path.show();
-            version.show();
-            repo.hide();
-            break;
-        case "repository":
-            path.hide();
-            version.hide();
-            repo.show();
+    if (value === "path") {
+        pathOption.style.display = "block";
+        versionOption.style.display = "none";
+        repoOption.style.display = "none";
+    } else if (value === "version") {
+        pathOption.style.display = "block";
+        versionOption.style.display = "block";
+        repoOption.style.display = "none";
+    } else {
+        pathOption.style.display = "none";
+        versionOption.style.display = "none";
+        repoOption.style.display = "block";
     }
 }
-
-jQuery3("div.arpParamType input.jenkins-radio__input").click(function() {
-    changeOptionsVisibility(this);
-});
-
-(function ($) {
-    const checkedOptions = jQuery3("div.arpParamType input.jenkins-radio__input:checked");
-    checkedOptions.each(function(index, element) {
-        changeOptionsVisibility(element);
-    });
-})(jQuery3);
